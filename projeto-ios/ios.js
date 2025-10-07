e907b2dc5fc076b860e8bb758de45898d8efd0d5
UlissesNunes <ulissesnunesx@gmail.com>
2025-10-06 21:22:03 -0300

    update

---
+++ projeto-ios/ios.js
@@ -0,0 +1,20 @@
+
+function tammanhoDiferente() {
+    if (window.innerWidth >= 768) {
+    itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+    }
+
+function ClickMenuinterativo() {
+if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+} else {
+    itens.style.display = 'block'
+}
+}
+
+
+
+
